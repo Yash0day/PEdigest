353d81e703c3df7e486264e5fd0563c499203ba1
Yash Kumar <yashchaudhary830@gmail.com>
2021-02-17 02:12:50 +0530

    Add files via upload

--- PEHeader.cpp
+++ PEHeader.cpp
@@ -10,7 +10,7 @@ int main() {
     PIMAGE_DOS_HEADER dosHeader;        //Pointer to DOS Header
     PIMAGE_NT_HEADERS ntHeader;         //Pointer to NT Header
     IMAGE_FILE_HEADER fileHeader;           //Pointer to image file header of NT Header 
-    IMAGE_OPTIONAL_HEADER opHeader;     //Optional Header of PE files present in NT Header structure
+    IMAGE_OPTIONAL_HEADER32 opHeader;     //Optional Header of PE files present in NT Header structure
     PIMAGE_SECTION_HEADER pSecHeader;   //Section Header or Section Table Header
     
 
@@ -64,15 +64,49 @@ int main() {
         ntHeader = PIMAGE_NT_HEADERS((DWORD)lpBase + dosHeader->e_lfanew);
         
         printf("\nxxxxxxxIMAGE NT HEADERxxxxxxx\n");
-        printf("ntHeader base offset: %X\n", (ntHeader)); //
+        printf("nt Header base offset: %X\n", (ntHeader)); //
         printf("Signature: %X\n", ntHeader->Signature); // 0x4550 ~ PE
 
-        printf("\t+++++++IMAGE FILE HEADER+++++++\n");
-        printf("\tMachine Architecture -  %X\n", ntHeader->FileHeader.Machine);  //0x014c ~ x86 architecture
+        printf("\n\t+++++++IMAGE FILE HEADER+++++++\n");
+        printf("\n\tMachine Architecture -  %X\n", ntHeader->FileHeader.Machine);  // 0x014c ~ x86 architecture
         printf("\tNumberOfSections - %X\n", ntHeader->FileHeader.NumberOfSections);
         printf("\tTimeDateStamp - %X\n", ntHeader->FileHeader.TimeDateStamp);
         printf("\tPointerToSymbolTable - %X\n", ntHeader->FileHeader.PointerToSymbolTable);
         printf("\tNumberOfSymbols - %X\n", ntHeader->FileHeader.NumberOfSymbols);
         printf("\tSizeOfOptionalHeader - %X\n", ntHeader->FileHeader.SizeOfOptionalHeader);
         printf("\tCharacteristics - %X\n", ntHeader->FileHeader.Characteristics);
+
+        printf("\n\t+++++++IMAGE OPTIONAL HEADER+++++++\n");
+        printf("\n\tMagic Number - %X\n", ntHeader->OptionalHeader.Magic); // 0x10b - File is an executable image
+        printf("\tThe major version number of the linker - %X\n", ntHeader->OptionalHeader.MajorLinkerVersion);
+        printf("\tThe minor version number of the linker - %X\n", ntHeader->OptionalHeader.MinorLinkerVersion);
+        printf("\tThe size of the code section - %X\n", ntHeader->OptionalHeader.SizeOfCode);
+        printf("\tThe size of the initialized data section - %X\n", ntHeader->OptionalHeader.SizeOfInitializedData);
+        printf("\tThe size of the uninitialized data section - %X\n", ntHeader->OptionalHeader.SizeOfUninitializedData);
+        printf("\tPointer to Entry Point of EXE - %X\n", ntHeader->OptionalHeader.AddressOfEntryPoint);
+        printf("\tpointer to the beginning of the code section - %X\n", ntHeader->OptionalHeader.BaseOfCode);
+        printf("\tpointer to the beginning of the data section - %X\n", ntHeader->OptionalHeader.BaseOfData);
+        printf("\tfirst byte of the image when it is loaded in memory - %X\n", ntHeader->OptionalHeader.ImageBase);
+        printf("\tThe alignment of sections loaded in memory - %X\n", ntHeader->OptionalHeader.SectionAlignment);
+        printf("\tThe alignment of the raw data of sections in the image file - %X\n", ntHeader->OptionalHeader.FileAlignment);
+        printf("\tThe major version number of the required operating system - %X\n", ntHeader->OptionalHeader.MajorOperatingSystemVersion);
+        printf("\tThe minor version number of the required operating system - %X\n", ntHeader->OptionalHeader.MinorOperatingSystemVersion);
+        printf("\tMajor Image Version - %X\n", ntHeader->OptionalHeader.MajorImageVersion);
+        printf("\tMinor Image Version - %X\n", ntHeader->OptionalHeader.MinorImageVersion);
+        printf("\tMajor Subsystem Version - %X\n", ntHeader->OptionalHeader.MajorSubsystemVersion);
+        printf("\tMinor Subsystem Version - %X\n", ntHeader->OptionalHeader.MinorSubsystemVersion);
+        printf("\tWin32 Version Value - %X\n", ntHeader->OptionalHeader.Win32VersionValue);
+        printf("\tThe size of the image - %X\n", ntHeader->OptionalHeader.SizeOfImage);
+        printf("\tThe combined size of all the Headers - %X\n", ntHeader->OptionalHeader.SizeOfHeaders);
+        printf("\tThe size of the image - %X\n", ntHeader->OptionalHeader.CheckSum);
+        printf("\tThe subsystem required to run this image - %X\n", ntHeader->OptionalHeader.Subsystem);
+        printf("\tThe DLL characteristics of the image - %X\n", ntHeader->OptionalHeader.DllCharacteristics);
+        printf("\tThe number of bytes to reserve for the stack - %X\n", ntHeader->OptionalHeader.SizeOfStackReserve);
+        printf("\tThe number of bytes to commit for the stack - %X\n", ntHeader->OptionalHeader.SizeOfStackCommit);
+        printf("\tThe number of bytes to reserve for the local heap - %X\n", ntHeader->OptionalHeader.SizeOfHeapReserve);
+        printf("\tThe number of bytes to commit for the local heap - %X\n", ntHeader->OptionalHeader.SizeOfHeapCommit);
+        printf("\tLoader Flags - %X\n", ntHeader->OptionalHeader.LoaderFlags);
+        printf("\tThe number of directory entries in the remainder of the optional header - %X\n", ntHeader->OptionalHeader.NumberOfRvaAndSizes);
+
+        printf("\n\t\t+++++++IMAGE DATA DIRECTORY+++++++\n");
 }
